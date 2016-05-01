4cb5bd7caa18d817b35ecb25e1115b2a4a6e96c0
Brandon Hay <brandon.a.hay@gmail.com>
2016-05-01 14:34:22 -0400

    testing what the sidebar looks like

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -27,14 +27,13 @@ Spree::BaseHelper.module_eval do
   end
 
   def taxons_tree(root_taxon, current_taxon, max_level = 1)
-    return '' if max_level < 1 || root_taxon.children.empty?
-    content_tag :ul, class: 'dropdown-menu', role: 'menu' do
-      root_taxon.children.map do |taxon|
-        content_tag :li do
-          css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'active' : ''
-          link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
-        end
-      end.join("\n").html_safe
+    return '' if max_level < 1 || root_taxon.leaf?
+    content_tag :div, class: 'list-group' do
+      taxons = root_taxon.children.map do |taxon|
+        css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item'
+        link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
+      end
+      safe_join(taxons, "\n")
     end
   end
 
