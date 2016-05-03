2a2b1bf187e60b50f89c87e86d80dc609f0ee42a
Brandon Hay <brandon.a.hay@gmail.com>
2016-05-02 21:58:09 -0400

    style changes

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -30,7 +30,7 @@ Spree::BaseHelper.module_eval do
     return '' if max_level < 1 || root_taxon.leaf?
     content_tag :div, class: 'list-group' do
       taxons = root_taxon.children.map do |taxon|
-        css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item'
+        css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item filter active' : 'list-group-item filter'
         link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
       end
       safe_join(taxons, "\n")
