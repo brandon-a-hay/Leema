ec0765cc0e800b74c3a6ec3e9c0cd47d1e21ca0b
Brandon Hay <brandon.a.hay@gmail.com>
2016-06-11 19:52:39 -0400

    dont show hidden products in autocomplete search and home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 	layout "home"
   def index
     # sort products by num of orders
-    @top_products = Spree::Product.all.sort_by { |p| p.sales_count }.reverse!
+    @top_products = Spree::Product.where(hidden: false).sort_by { |p| p.sales_count }.reverse!
     # get randomized list of the top 10
     @top_10_products = @top_products.first(10).shuffle
     # show on product home page
@@ -12,8 +12,8 @@ class WelcomeController < ApplicationController
 
     # predictive search shizzz
     @product_names = []
-    @allProducts = Spree::Product.all
-    @allProducts.each do |product|
+    @available_products = Spree::Product.where(hidden: false)
+    @available_products.each do |product|
       @product_names << product.name
     end
     File.open("public/productnames.json","w") do |f|
