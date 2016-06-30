2ae493eefdf9478308bf714469ecd233082b4cc3
Brandon Hay <brandon.a.hay@gmail.com>
2016-06-30 16:23:29 -0400

    try overwriting the override

--- app/overrides/add_reviews_after_product_properties.rb
+++ app/overrides/add_reviews_after_product_properties.rb
@@ -1,5 +1,1 @@
-Deface::Override.new(:virtual_path => "spree/products/show",
-                     :name => "converted_product_properties_767643482",
-                     :insert_after => "[data-hook='promotions']",
-                     :partial => "spree/product/reviews",
-                     :disabled => true)
+#
