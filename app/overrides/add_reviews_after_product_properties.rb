c9d08166c06b5cd00ff83948aa684696a00cdb30
Brandon Hay <brandon.a.hay@gmail.com>
2016-06-30 16:19:12 -0400

    put reviews in product div to reduce whitespace

---
+++ app/overrides/add_reviews_after_product_properties.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/products/show",
+                     :name => "converted_product_properties_767643482",
+                     :insert_after => "[data-hook='promotions']",
+                     :partial => "spree/product/reviews",
+                     :disabled => true)
