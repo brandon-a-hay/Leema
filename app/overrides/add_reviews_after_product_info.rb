1088242d9ab631d7de7b4eeb4a79fa2db1d43a0e
Brandon Hay <brandon.a.hay@gmail.com>
2016-06-30 16:40:05 -0400

    fix the reviews to go on the left side of the page

--- app/overrides/add_reviews_after_product_info.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/products/show",
-                     :name => "converted_product_properties_767643482",
-                     :insert_after => "#product-info-page",
-                     :partial => "spree/shared/reviews",
-                     :disabled => false)
